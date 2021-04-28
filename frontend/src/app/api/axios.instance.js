2c757c8aab6f38f361e48c6e136a379910f2abce
Mahran Omairy <mahran@thelucky.app>
2021-04-28 21:55:01 +0200

    remove api link

--- frontend/src/app/api/axios.instance.js
+++ frontend/src/app/api/axios.instance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://stockapi20200306052122.azurewebsites.net/api/",
-  headers: { language: "en" }
+  baseURL: "LINK TO API",
+  headers: { language: "en" },
 });
 
 export default instance;
