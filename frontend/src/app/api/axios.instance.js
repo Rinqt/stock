5fb12fdea1505af2eeae1d70734f78c3920cb208
Ilda <ilda.balliu@sap.com>
2021-04-29 22:26:31 +0200

    change api endpoint

--- frontend/src/app/api/axios.instance.js
+++ frontend/src/app/api/axios.instance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://stockapi20200306052122.azurewebsites.net/api/",
-  headers: { language: "en" }
+	baseURL: "https://stockapi29042021.azurewebsites.net/api/",
+	headers: { language: "en" },
 });
 
 export default instance;
